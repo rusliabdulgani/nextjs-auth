64b8e8dec78d746f709f728497ec25f48b0469a4
rusli abdul gani <abdul.gani@fullertonhealth.com>
2024-03-15 15:38:05 +0700

    update

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,21 @@
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { LoginSchema } from "@/schemas"
 import { CardWrapper } from "@/components/auth"
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>()
+
+
   return (
     <CardWrapper 
       headerLabel="Wellcome back!"
